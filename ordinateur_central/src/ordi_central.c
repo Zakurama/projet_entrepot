b831a55282c8ec8480c938467e9b443dced3d694
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2025-03-02 18:52:02 +0100

    fonction trajectoire : ajout des bac + fonctions tests

--- ordinateur_central/src/ordi_central.c
+++ ordinateur_central/src/ordi_central.c
@@ -19,102 +19,132 @@ void trajectoire(const char* pos_initiale, const char* pos_finale, char path[MAX
     if(!(((type_pos_finale=='S') && (pos_finale_index%(NB_COLONNES + 1) != 0) && (pos_finale_index % (2*(NB_COLONNES + 1))<(NB_COLONNES + 1)) ) || (type_pos_finale!='S' && pos_finale_index%(NB_COLONNES + 1) == 0))){
         printf("La position finale donnée est non cohérente. Si de type S : ne doit pas être mutliple de %d, sinon doit être mutliple de %d\nSi de type S, il faut aussi que pos_finale_index mod %d<%d\n",NB_COLONNES + 1,NB_COLONNES + 1,2*(NB_COLONNES + 1),NB_COLONNES + 1);
         exit(EXIT_FAILURE);
+    }            
+    
+    strcpy(path[0], pos_initiale);
+    int i = 1;
+
+    if(type_pos=='B'){
+        // Si le début c'est un bac alors on retourne en PXX avant de repartir
+        if(pos_index == 5){
+            // On s'avance sur le deuxième bac
+            strcpy(path[i], "B10");
+            i++;
+        }
+        strcpy(path[i], "P25");
+        i++;
+        type_pos = 'P';
+        pos_index = 25;
     }
+    
+    while (pos_index!=pos_finale_index || type_pos!=type_pos_finale){
+        char holder_name_place[5];
+        
+        if(type_pos == 'P'){
+            // On est au parking
+            // On doit avancer sur la colone de montée
+            sprintf(holder_name_place, "D%d", pos_index);
+            strcpy(path[i], holder_name_place);
+        }
 
-    // On vérifie que la position finale donnée est cohérente
-    if(((type_pos_finale=='S') && (pos_finale_index%(NB_COLONNES + 1) != 0) && (pos_finale_index % (2*(NB_COLONNES + 1))<(NB_COLONNES + 1)) ) || (type_pos_finale!='S' && pos_finale_index%(NB_COLONNES + 1) == 0)){
-            
-        strcpy(path[0], pos_initiale);
-        int i = 1;
-        while (pos_index!=pos_finale_index){
-            char holder_name_place[5];
+        else if (pos_index > pos_finale_index){
+            // On regarde ou on est
+            if (type_pos == 'D'){
+                // On est sur la colone de descente
+                // On doit avancer sur la colone de montée
+                sprintf(holder_name_place, "M%d", pos_index);
+                strcpy(path[i], holder_name_place);
+            }
+           else if (type_pos == 'M'){
+                // On est sur la colone de montée
+                // On peut monter
+                if(pos_index + NB_COLONNES > pos_finale_index){
+                    sprintf(holder_name_place, "M%d", pos_index-(NB_COLONNES + 1));
+                    strcpy(path[i], holder_name_place);
+                }
+            }
+            else if (type_pos == 'S'){
+                // On va vers la gauche
+                if((pos_index)%(NB_COLONNES + 1)==0){
+                    // On est arrivé en bout (début) des étagères
+                    sprintf(holder_name_place, "M%d", pos_index);
+                    strcpy(path[i], holder_name_place);
+                }
+                else{
+                    sprintf(holder_name_place, "S%d", pos_index-1);
+                    strcpy(path[i], holder_name_place);
+                }
+            }
+        }
+                
+        else if(pos_index +  NB_COLONNES >= pos_finale_index){
+            // Je suis a la bonne altitude (on est devant la bonne étagère de stocks)
+            if(pos_index < pos_finale_index && ((type_pos=='M')||(type_pos=='S'))){
+                // On va vers la droite
+                sprintf(holder_name_place, "S%d", pos_index+1);
+                strcpy(path[i], holder_name_place);
+            }
+            // Je suis a la bonne altitude (on est devant le bon étage M)
+            if(pos_index < pos_finale_index && ((type_pos=='D'))){
+                // On va sur la colone de montée
+                sprintf(holder_name_place, "M%d", pos_index);
+                strcpy(path[i], holder_name_place);
+            }
+        }
 
-            if(type_pos == 'P'){
-                // On est au parking
+        else if (pos_index < pos_finale_index){
+            // On regarde ou on est
+            if (type_pos == 'D'){
+                // On est sur la colone de descente 
+                // On peut descendre
+                if(pos_index + NB_COLONNES +1<= pos_finale_index){
+                    sprintf(holder_name_place, "D%d", pos_index+(NB_COLONNES + 1));
+                    strcpy(path[i], holder_name_place);
+                }
+            }
+            else if (type_pos == 'M'){
+                // On est sur la colone de montée
                 // On doit avancer sur la colone de montée
                 sprintf(holder_name_place, "D%d", pos_index);
                 strcpy(path[i], holder_name_place);
             }
-
-            else if (pos_index > pos_finale_index){
-                // On regarde ou on est
-                if (type_pos == 'D'){
-                    // On est sur la colone de descente
-                    // On doit avancer sur la colone de montée
+            else if (type_pos == 'S'){
+                // On va vers la gauche
+                if((pos_index)%(NB_COLONNES + 1)==0){
+                    // On est arrivé en bout (début) des étagères
                     sprintf(holder_name_place, "M%d", pos_index);
                     strcpy(path[i], holder_name_place);
                 }
-                else if (type_pos == 'M'){
-                    // On est sur la colone de montée
-                    // On peut monter
-                    if(pos_index + NB_COLONNES > pos_finale_index){
-                        sprintf(holder_name_place, "M%d", pos_index-(NB_COLONNES + 1));
-                        strcpy(path[i], holder_name_place);
-                    }
-                }
-                else if (type_pos == 'S'){
-                    // On va vers la gauche
-                    if((pos_index)%(NB_COLONNES + 1)==0){
-                        // On est arrivé en bout (début) des étagères
-                        sprintf(holder_name_place, "M%d", pos_index);
-                        strcpy(path[i], holder_name_place);
-                    }
-                    else{
-                        sprintf(holder_name_place, "S%d", pos_index-1);
-                        strcpy(path[i], holder_name_place);
-                    }
-                }
-            }
-                
-            else if(pos_index +  NB_COLONNES >= pos_finale_index){
-                // Je suis a la bonne altitude (on est devant la bonne étagère de stocks)
-                if(pos_index < pos_finale_index && ((type_pos=='M')||(type_pos=='S'))){
-                    // On va vers la droite
-                    sprintf(holder_name_place, "S%d", pos_index+1);
-                    strcpy(path[i], holder_name_place);
-                }
-                // Je suis a la bonne altitude (on est devant le bon étage M)
-                if(pos_index < pos_finale_index && ((type_pos=='D'))){
-                    // On va sur la colone de montée
-                    sprintf(holder_name_place, "M%d", pos_index);
+                else{
+                    sprintf(holder_name_place, "S%d", pos_index-1);
                     strcpy(path[i], holder_name_place);
                 }
             }
 
-
-            else if (pos_index < pos_finale_index){
-                // On regarde ou on est
-                if (type_pos == 'D'){
-                    // On est sur la colone de descente 
-                    // On peut descendre
-                    if(pos_index + NB_COLONNES +1<= pos_finale_index){
-                        sprintf(holder_name_place, "D%d", pos_index+(NB_COLONNES + 1));
-                        strcpy(path[i], holder_name_place);
-                    }
-                }
-                else if (type_pos == 'M'){
-                    // On est sur la colone de montée
-                    // On doit avancer sur la colone de montée
-                    sprintf(holder_name_place, "D%d", pos_index);
-                    strcpy(path[i], holder_name_place);
-                }
-                else if (type_pos == 'S'){
-                    // On va vers la gauche
-                    if((pos_index)%(NB_COLONNES + 1)==0){
-                        // On est arrivé en bout (début) des étagères
-                        sprintf(holder_name_place, "M%d", pos_index);
-                        strcpy(path[i], holder_name_place);
-                    }
-                    else{
-                        sprintf(holder_name_place, "S%d", pos_index-1);
-                        strcpy(path[i], holder_name_place);
-                    }
-                }
-
-            }
-            pos_index = atoi(path[i]+1);
-            type_pos = path[i][0];
-            i++;
         }
+        if(pos_index == pos_finale_index){
+            if((type_pos=='D') && (type_pos_finale=='P')){
+                // Je suis au bon niveau mais je veux aller au parking
+                // On se déplace
+                sprintf(holder_name_place, "P%d" ,pos_index);
+                strcpy(path[i], holder_name_place);
+            }
+            if((type_pos=='M') && (type_pos_finale=='B')){
+                // Je suis au bon niveau mais je veux aller sur les bacs
+                // On se déplace
+                sprintf(holder_name_place, "D%d" ,pos_index);
+                strcpy(path[i], holder_name_place);
+            }
+            if((type_pos=='D') && (type_pos_finale=='B')){
+                // Je suis au bon niveau mais je veux aller aux bacs
+                // On se déplace
+                sprintf(holder_name_place, "B%d" ,pos_index);
+                strcpy(path[i], holder_name_place);
+            }
+        }
+
+        pos_index = atoi(path[i]+1);
+        type_pos = path[i][0];
+        i++;
     }
 }
--- ordinateur_central/tests/test.c
+++ ordinateur_central/tests/test.c
@@ -7,7 +7,7 @@
 
 #include "ordi_central.h"
 
-void test_trajectoire(const char *pos_initiale, const char *pos_finale) {
+void test_trajectoire_generique(const char *pos_initiale, const char *pos_finale, char expected[][SIZE_POS]) {
     char path[MAX_WAYPOINTS][SIZE_POS];
 
     // Initialisation des éléments du tableau
@@ -18,36 +18,100 @@ void test_trajectoire(const char *pos_initiale, const char *pos_finale) {
     trajectoire(pos_initiale, pos_finale, path);
 
     for (int i = 0; i < MAX_WAYPOINTS; i++) {
-        if (strcmp(path[i], "\0") != 0) {
-            if (i == 0) {
-                printf("%s", path[i]);
-            } else {
-                printf(" -> %s", path[i]);
-            }
-        } else {
-            printf("\n");
+        if (expected[i][0] == '\0' && path[i][0] == '\0') {
             break;
         }
+        CU_ASSERT_STRING_EQUAL(path[i], expected[i]);
     }
 }
 
+void test_trajectoire_D25_S14() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "D25", "M25", "M20", "M15", "M10","S11","S12","S13","S14" 
+    };
+    test_trajectoire_generique("D25", "S14", expected);
+}
+void test_trajectoire_S14_S11() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "S14", "S13", "S12", "S11"
+    };
+    test_trajectoire_generique("S14", "S11", expected);
+}
+
+void test_trajectoire_P25_S44() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "P25", "D25", "D30", "D35", "D40", "M40", "S41", "S42", "S43", "S44"
+    };
+    test_trajectoire_generique("P25", "S44", expected);
+}
+
+void test_trajectoire_S44_S11() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "S44", "S43", "S42", "S41", "S40", "M40", "M35", "M30", "M25", "M20", "M15", "M10", "S11"
+    };
+    test_trajectoire_generique("S44", "S11", expected);
+}
+
+void test_trajectoire_S11_S44() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "S11", "S10", "M10", "D10", "D15", "D20", "D25", "D30", "D35", "D40", "M40", "S41", "S42", "S43", "S44"
+    };
+    test_trajectoire_generique("S11", "S44", expected);
+}
+
+void test_trajectoire_S14_P25() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "S14", "S13", "S12", "S11", "S10", "M10", "D10", "D15", "D20", "D25", "P25"
+    };
+    test_trajectoire_generique("S14", "P25", expected);
+}
+
+void test_trajectoire_P25_B5() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "P25", "D25", "M25", "M20", "M15", "M10", "M5", "D5", "B5"
+    };
+    test_trajectoire_generique("P25", "B5", expected);
+}
+
+void test_trajectoire_B5_P25() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "B5", "B10", "P25"
+    };
+    test_trajectoire_generique("B5", "P25", expected);
+}
+
+void test_trajectoire_B10_P25() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "B10", "P25"
+    };
+    test_trajectoire_generique("B10", "P25", expected);
+}
+
+void test_trajectoire_D30_B10() {
+    char expected[MAX_WAYPOINTS][SIZE_POS] = {
+        "D30", "M30", "M25", "M20", "M15", "M10", "D10", "B10"
+    };
+    test_trajectoire_generique("D30", "B10", expected);
+}
 int main() {
-    printf("Trajectoire de D25 à S14\n");
-    test_trajectoire("D25", "S14");
-    
-    printf("Trajectoire de S14 à S11\n");
-    test_trajectoire("S14", "S11");
+    CU_initialize_registry();
 
-    printf("Trajectoire de P25 à S44\n");
-    test_trajectoire("P25", "S44");
+    CU_pSuite suite = CU_add_suite("Tests Trajectoire", NULL, NULL);
 
-    printf("Trajectoire de S44 à S11\n");
-    test_trajectoire("S44", "S11");
+    CU_add_test(suite, "Trajectoire de D25 à S14", test_trajectoire_D25_S14);
+    CU_add_test(suite, "Trajectoire de S14 à S11", test_trajectoire_S14_S11);
+    CU_add_test(suite, "Trajectoire de P25 à S44", test_trajectoire_P25_S44);
+    CU_add_test(suite, "Trajectoire de S44 à S11", test_trajectoire_S44_S11);
+    CU_add_test(suite, "Trajectoire de S11 à S44", test_trajectoire_S11_S44);
+    CU_add_test(suite, "Trajectoire de S14 à P25", test_trajectoire_S14_P25);
+    CU_add_test(suite, "Trajectoire de P25 à S11", test_trajectoire_P25_B5);
+    CU_add_test(suite, "Trajectoire de B5 à P25", test_trajectoire_B5_P25);
+    CU_add_test(suite, "Trajectoire de B10 à P25", test_trajectoire_B10_P25);
+    CU_add_test(suite, "Trajectoire de D30 à B10", test_trajectoire_D30_B10);
 
-    printf("Trajectoire de S11 à S44\n");
-    test_trajectoire("S11", "S44");
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
 
-    printf("Trajectoire de S14 à D40\n");
-    test_trajectoire("S14", "D40");
     return 0;
 }
