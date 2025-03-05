8e11492d7e0297947dc95ddd459f8543be57b70c
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2025-03-05 14:51:38 +0100

    modif trajectoire entrepot

--- tests/test_ordinateur_central.c
+++ tests/test_ordinateur_central.c
@@ -28,7 +28,7 @@ void test_trajectoire_generique(const char *pos_initiale, const char *pos_finale
 
 void test_trajectoire_D25_S14() {
     char expected[MAX_WAYPOINTS][SIZE_POS] = {
-        "D25", "M25", "M20", "M15", "M10","S11","S12","S13","S14" 
+        "D25","D30","D35","D40","M40","M35","M30", "M25", "M20", "M15", "M10","S11","S12","S13","S14" 
     };
     test_trajectoire_generique("D25", "S14", expected);
 }
@@ -55,21 +55,21 @@ void test_trajectoire_S44_S11() {
 
 void test_trajectoire_S11_S44() {
     char expected[MAX_WAYPOINTS][SIZE_POS] = {
-        "S11", "S10", "M10", "D10", "D15", "D20", "D25", "D30", "D35", "D40", "M40", "S41", "S42", "S43", "S44"
+        "S11", "S10", "M10","M5","D5", "D10", "D15", "D20", "D25", "D30", "D35", "D40", "M40", "S41", "S42", "S43", "S44"
     };
     test_trajectoire_generique("S11", "S44", expected);
 }
 
 void test_trajectoire_S14_P25() {
     char expected[MAX_WAYPOINTS][SIZE_POS] = {
-        "S14", "S13", "S12", "S11", "S10", "M10", "D10", "D15", "D20", "D25", "P25"
+        "S14", "S13", "S12", "S11", "S10", "M10","M5","D5", "D10", "D15", "D20", "D25", "P25"
     };
     test_trajectoire_generique("S14", "P25", expected);
 }
 
 void test_trajectoire_P25_B5() {
     char expected[MAX_WAYPOINTS][SIZE_POS] = {
-        "P25", "D25", "M25", "M20", "M15", "M10", "M5", "D5", "B5"
+        "P25", "D25","D30","D35","D40","M40","M35","M30", "M25", "M20", "M15", "M10", "M5", "D5", "B5"
     };
     test_trajectoire_generique("P25", "B5", expected);
 }
@@ -90,7 +90,7 @@ void test_trajectoire_B10_P25() {
 
 void test_trajectoire_D30_B10() {
     char expected[MAX_WAYPOINTS][SIZE_POS] = {
-        "D30", "M30", "M25", "M20", "M15", "M10", "D10", "B10"
+        "D30","D35","D40","M40","M35", "M30", "M25", "M20", "M15", "M10", "D10", "B10"
     };
     test_trajectoire_generique("D30", "B10", expected);
 }
@@ -344,7 +344,7 @@ int main() {
     CU_add_test(suite, "Trajectoire de S44 à S11", test_trajectoire_S44_S11);
     CU_add_test(suite, "Trajectoire de S11 à S44", test_trajectoire_S11_S44);
     CU_add_test(suite, "Trajectoire de S14 à P25", test_trajectoire_S14_P25);
-    CU_add_test(suite, "Trajectoire de P25 à S11", test_trajectoire_P25_B5);
+    CU_add_test(suite, "Trajectoire de P25 à B5", test_trajectoire_P25_B5);
     CU_add_test(suite, "Trajectoire de B5 à P25", test_trajectoire_B5_P25);
     CU_add_test(suite, "Trajectoire de B10 à P25", test_trajectoire_B10_P25);
     CU_add_test(suite, "Trajectoire de D30 à B10", test_trajectoire_D30_B10);
