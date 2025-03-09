9366d4fc65c301abac8a8341180a27ba87506717
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2025-03-09 11:52:58 +0100

    ajout d'un gestionnaire de flotte pour gérer un nombre dynamique de robots

--- ordinateur_central/include/ordi_central.h
+++ ordinateur_central/include/ordi_central.h
@@ -19,7 +19,7 @@
 #include <sys/mman.h>
 
 #define SIZE_POS 10
-#define NB_ROBOT 2
+#define NB_MAX_ROBOT 10
 #define DEFAULT_NB_COLONNES 4
 #define DEFAULT_NB_LIGNES 4
 #define MAX_ARTICLES_PORTES 2
@@ -48,7 +48,6 @@ typedef struct {
 } Robot;
 
 void trajectoire(const char* pos_initiale, const char* pos_finale, char path[MAX_WAYPOINTS][SIZE_POS]);
-void gestionnaire_inventaire(int se);
 char *parse_stock(const char *request, int max_elements, int *L_n[max_elements], int *L_x[max_elements], int *L_y[max_elements], char *item_names[max_elements], int count[max_elements], int *nb_items_request);
 int choose_items_stocks(char *item_names_requested[], int L_n_requested[], int count_requested,char *item_names_stock[], int *L_n_stock[], int *L_x_stock[], int *L_y_stock[], int count_stock[],Item_selected selected_items[]);
 void update_shared_memory_stock(Robot *shared_memory,Item_selected selected_items,int index_pos);
